34b102232ed8cebe71e66617b027eb3fbb935df9
Oliver Grabinski <20188437+olivergrabinski@users.noreply.github.com>
2025-07-21 23:43:20 +0200

    fix for arm macos 15

--- openvaf/target/src/spec/aarch64_apple_darwin.rs
+++ openvaf/target/src/spec/aarch64_apple_darwin.rs
@@ -2,7 +2,9 @@ use crate::spec::{LinkerFlavor, Target, TargetOptions};
 
 pub fn target() -> Target {
     let mut base = super::apple_base::opts();
-    base.cpu = "apple-a14".to_string();
+    base.cpu = "apple-m1".to_string();
+
+    let sdk_path = "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk";
 
     base.pre_link_args.insert(
         LinkerFlavor::Ld64,
@@ -11,6 +13,9 @@ pub fn target() -> Target {
             "arm64".to_string(),
             "-undefined".to_string(),
             "dynamic_lookup".to_string(),
+            "-lSystem".to_string(),
+            "-syslibroot".to_string(),
+            sdk_path.to_string(),
         ],
     );
 
